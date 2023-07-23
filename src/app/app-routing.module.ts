d2b8f1831dec7ceeb6f52e0dbf758179ee5538e1
Dmitrii Danilov <d.danilov@strongte.am>
2023-07-23 12:50:08 +0600

    feature/list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [{
+  path: 'cars-news',
+  loadComponent: () => import('./cars-news/cars-news-list.component').then(c => c.CarsNewsListComponent)
+},{
+  path: '**',
+  redirectTo: 'cars-news'
+}];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/core/api.ts
@@ -0,0 +1,1 @@
+export const BASE_URL = 'https://webapi.autodoc.ru/api/news';
